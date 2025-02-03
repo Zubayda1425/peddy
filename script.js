cb9310d2de8af82b355a87c702b1e63f54cf7000
Jobaida20 <zubayda.gulshan.ara@gmail.com>
2025-02-03 11:10:56 +0600

    complete_Navigation

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ console.log("connected");
 
 function toggleMenu() {
     const menu = document.getElementById("menu");
-    menu.classList.toggle("w-[70vw]");
+    menu.classList.toggle("w-[50vw]");
 }
